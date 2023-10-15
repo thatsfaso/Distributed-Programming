6ddca4df3a7df22b612c679beb83d7068611ca61
Iliano Fasolino <127889824+thatsfaso@users.noreply.github.com>
2023-10-15 18:49:45 +0200

    Create HelloImpl.java

---
+++ HelloRMI/HelloImpl.java
@@ -0,0 +1,27 @@
+package it.unisa.studenti;
+
+import java.rmi. *;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.logging.Logger;
+public class HelloImpl extends UnicastRemoteObject implements Hello {
+    private static final long serialVersionUID = -4469091140865645865L;
+    static Logger logger= Logger.getLogger("global");
+    public HelloImpl()throws RemoteException { //vuoto
+    }
+    public String dimmiQualcosa(String who)
+            throws RemoteException {
+        logger.info("Sto salutando"+who);
+        return "Ciao!"; }
+
+    public static void main(String args[]) { System.setSecurityManager(new RMISecurityManager());
+        try{
+            logger.info("Creo l’oggetto remoto...");
+            HelloImpl obj = new HelloImpl();
+            logger.info("...ne effettuo il rebind...");
+            Naming.rebind("HelloServer", obj);
+            logger.info("...Pronto!");
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }//end main
+}//end classe HelloImpl
